a9e1e9a703407e0e1e42b027bcc1e1ad0986d315
William Morgan <wmorgan-github@masanjin.net>
2012-05-12 13:59:51 -0700

    change processing queue to a set, and add a descriptor
    Return items from the processing set with a timestamp and this
    consumer-supplied descriptor. This makes error recovery actually
    possible.

--- lib/redis-scheduler.rb
+++ lib/redis-scheduler.rb
@@ -19,7 +19,7 @@ class RedisScheduler
     @blocking = opts[:blocking]
 
     @queue = [@namespace, "q"].join
-    @error_queue = [@namespace, "errorq"].join
+    @processing_set = [@namespace, "processing"].join
     @counter = [@namespace, "counter"].join
   end
 
@@ -31,27 +31,34 @@ class RedisScheduler
   end
 
   def reset!
-    [@queue, @error_queue, @counter].each { |k| @redis.del k }
+    [@queue, @processing_set, @counter].each { |k| @redis.del k }
   end
 
   def size; @redis.zcard @queue end
-  def error_queue_size; @redis.llen @error_queue end
 
-  ## yields items along with their scheduled times. only returns items
-  ## on or after their scheduled times. items returned as strings. if
-  ## @blocking is false, will stop once there are no more items that can
-  ## be processed immediately; if it's true, will wait until items
-  ## become available (and never terminate).
-  def each
-    while(x = get)
-      item, erritem, at = x
+  ## Returns the total number of items currently being processed.
+  def processing_set_size; @redis.scard @processing_set end
+
+  ## Yields items along with their scheduled times. only returns items on or
+  ## after their scheduled times. items are returned as strings. if @blocking is
+  ## false, will stop once there are no more items that can be processed
+  ## immediately; if it's true, will wait until items become available (and
+  ## never terminate).
+  ##
+  ## +Descriptor+ is an optional string that will be associated with this item
+  ## while in the processing set. This is useful for providing whatever
+  ## information you need to determine whether the item needs to be recovered
+  ## when iterating through the processing set.
+  def each descriptor=nil
+    while(x = get(descriptor))
+      item, processing_descriptor, at = x
       begin
         yield item, at
       rescue Exception # back in the hole!
         schedule! item, at
         raise
       ensure
-        cleanup! erritem
+        cleanup! processing_descriptor
       end
     end
   end
@@ -65,38 +72,48 @@ class RedisScheduler
   ## for these reasons, this operation is mainly useful for debugging purposes.
   def items; ItemEnumerator.new(@redis, @queue) end
 
+  ## Returns an Array of [item, timestamp, descriptor] tuples representing the
+  ## set of in-process items. The timestamp corresponds to the time at which
+  ## the item was removed from the schedule for processing.
+  def processing_set_items
+    @redis.smembers(@processing_set).map do |x|
+      item, timestamp, descriptor = Marshal.load(x)
+      [item, Time.at(timestamp), descriptor]
+    end
+  end
+
 private
 
-  def get; @blocking ? blocking_get : nonblocking_get end
+  def get descriptor; @blocking ? blocking_get(descriptor) : nonblocking_get(descriptor) end
 
-  def blocking_get
-    sleep POLL_DELAY until(x = nonblocking_get)
+  def blocking_get descriptor
+    sleep POLL_DELAY until(x = nonblocking_get(descriptor))
     x
   end
 
   class InvalidEntryException < StandardError; end
-  def nonblocking_get
+  def nonblocking_get descriptor
     catch :cas_retry do
       @redis.watch @queue
-      item, at = @redis.zrangebyscore @queue, 0, Time.now.to_f,
-        :withscores => true, :limit => [0, 1]
-      if item
+      entry, at = @redis.zrangebyscore @queue, 0, Time.now.to_f, :withscores => true, :limit => [0, 1]
+      if entry
+        entry =~ /^\d+:(\S+)$/ or raise InvalidEntryException, entry
+        item = $1
+        processing_descriptor = Marshal.dump [item, Time.now.to_i, descriptor]
         @redis.multi do # try and grab it
-          @redis.zrem @queue, item
-          @redis.lpush @error_queue, item
+          @redis.zrem @queue, entry
+          @redis.sadd @processing_set, processing_descriptor
         end or begin
           sleep CAS_DELAY
           throw :cas_retry
         end
-        item =~ /^\d+:(\S+)$/ or raise InvalidEntryException, item
-        original = $1
-        [original, item, Time.at(at.to_f)]
+        [item, processing_descriptor, Time.at(at.to_f)]
       end
     end
   end
 
   def cleanup! item
-    @redis.lrem @error_queue, 1, item
+    @redis.srem @processing_set, item
   end
 
   ## enumerable for just iterating over everything in the queue
